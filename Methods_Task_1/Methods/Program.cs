e7e478f0b70b35f610b7a16b187990d283081072
IbrahimDev04 <tu7nluy9f@code.edu.az>
2024-03-18 19:21:57 +0400

    add content

---
+++ Methods_Task_1/Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿//Method Task: Min methodu yaradılır. Daxil olunan arrayın ən kiçik elementini tapır.
+
+
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 43, 42, 112, 65, 856, 234, 5433, 3234};
+            Console.WriteLine(CalLower(arr));
+        }
+
+        public static int CalLower(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ Methods_Task_2/Methods_Task_2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Methods_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Çevrənin sahəsi 
+            Console.WriteLine(Area(3));
+
+            //Düzbucaqlının sahəsi
+            Console.WriteLine(Area(4, 5));
+
+            //Düzbucaqlı paralelpipedin tam səthinin sahəsi
+            Console.WriteLine(Area(2, 5, 7));
+
+            //Düzbucaqlı paralelpipedin tam səthinin sahəsi
+            Console.WriteLine(Area(5, 2, 4, 6));
+
+        }
+
+
+        //Çevrənin sahəsi 
+        public static int Area(int r)
+        {
+            int area = 3 * r * r;
+            return area;
+
+        }
+
+
+
+        //Düzbucaqlının sahəsi
+        public static int Area(int a, int b)
+        {
+            int area = a * b;
+            return area;
+
+        }
+
+
+
+        //Düzbucaqlı paralelpipedin tam səthinin sahəsi
+        public static int Area(int a, int b, int c)
+        {
+            int area = 2 * (a * b + b * c + a * c);
+            return area;
+
+        }
+
+
+
+        //Üçbucaqlının daxilinə çəkilmiş çevrənin sahəsi
+        public static int Area(int r, int a, int b, int c)
+        {
+            int area = (a+b+c)/2*r;
+            return area;
+
+        }
+    }
+}
